825e0acaa1ffdca698f46f79391ea9809b53a9fe
Lex Mel <lex07672@gmail.com>
2024-05-16 21:16:28 +0300

    fix: revork copy and stop func for reduce code dupblicate

--- src/pages/page-registration/copy-billing-to-shipping.ts
+++ src/pages/page-registration/copy-billing-to-shipping.ts
@@ -1,53 +1,32 @@
-let inputEventHandler: () => void;
+const inputEventHandlers: { [key: string]: () => void } = {};
 
-export function copyBillingToShipping() {
-  const billingCity = document.getElementById('form-city') as HTMLInputElement;
-  const shippingCity = document.getElementById('ship-city') as HTMLInputElement;
-  if (billingCity && shippingCity) {
-    inputEventHandler = () => {
-      shippingCity.value = billingCity.value;
-    };
-    shippingCity.value = billingCity.value;
-    billingCity.addEventListener('input', inputEventHandler);
-  }
+function startCopy(billingAddrId: string, shippingAddrId: string) {
+  const billingAddr = document.getElementById(billingAddrId) as HTMLInputElement;
+  const shippingAddr = document.getElementById(shippingAddrId) as HTMLInputElement;
 
-  const billingStreet = document.getElementById('form-street') as HTMLInputElement;
-  const shippingStreet = document.getElementById('ship-street') as HTMLInputElement;
-  if (billingStreet && shippingStreet) {
-    inputEventHandler = () => {
-      shippingStreet.value = billingStreet.value;
+  if (billingAddr && shippingAddr) {
+    const inputEventHandler = () => {
+      shippingAddr.value = billingAddr.value;
     };
-    shippingStreet.value = billingStreet.value;
-    billingStreet.addEventListener('input', inputEventHandler);
-  }
-
-  const billingPostal = document.getElementById('form-postal') as HTMLInputElement;
-  const shippingPostal = document.getElementById('ship-postal') as HTMLInputElement;
-  if (billingPostal && shippingPostal) {
-    inputEventHandler = () => {
-      shippingPostal.value = billingPostal.value;
-    };
-    shippingPostal.value = billingPostal.value;
-    billingPostal.addEventListener('input', inputEventHandler);
+    inputEventHandlers[billingAddrId] = inputEventHandler;
+    shippingAddr.value = billingAddr.value;
+    billingAddr.addEventListener('input', inputEventHandler);
   }
 }
 
+export function copyBillingToShipping() {
+  startCopy('form-city', 'ship-city');
+  startCopy('form-street', 'ship-street');
+  startCopy('form-postal', 'ship-postal');
+}
+
 export function stopCopy() {
-  const billingCity = document.getElementById('form-city') as HTMLInputElement;
-  const shippingCity = document.getElementById('ship-city') as HTMLInputElement;
-  if (billingCity && shippingCity) {
-    billingCity.removeEventListener('input', inputEventHandler);
-  }
-
-  const billingStreet = document.getElementById('form-street') as HTMLInputElement;
-  const shippingStreet = document.getElementById('ship-street') as HTMLInputElement;
-  if (billingStreet && shippingStreet) {
-    billingStreet.removeEventListener('input', inputEventHandler);
-  }
-
-  const billingPostal = document.getElementById('form-postal') as HTMLInputElement;
-  const shippingPostal = document.getElementById('ship-postal') as HTMLInputElement;
-  if (billingPostal && shippingPostal) {
-    billingPostal.removeEventListener('input', inputEventHandler);
-  }
+  const billingAddrIds = Object.keys(inputEventHandlers);
+  billingAddrIds.forEach((billingAddrId) => {
+    const billingAddr = document.getElementById(billingAddrId) as HTMLInputElement;
+    const inputEventHandler = inputEventHandlers[billingAddrId];
+    if (billingAddr && inputEventHandler) {
+      billingAddr.removeEventListener('input', inputEventHandler);
+    }
+  });
 }
