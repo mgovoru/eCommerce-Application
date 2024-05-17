f0ddf123d27e354f71ebbc785246952341548c73
Lex Mel <lex07672@gmail.com>
2024-05-17 13:37:07 +0300

    fix: small fixes

--- src/pages/page-login/validation.ts
+++ src/pages/page-login/validation.ts
@@ -1,10 +1,10 @@
 import { ElementCreator } from '../../app/base';
 
-const MIN_LOGIN_LENGTH = 3;
+// const MIN_LOGIN_LENGTH = 3; // сообщение о ошибке 'Username must start with a capital letter and be at least 3 characters long'
 const MIN_PASSWORD_LENGTH = 8;
 
 const isEmailValid = (value: string) => {
-  const pattern = new RegExp(`^[A-ZА-Я][a-zA-Zа-яА-Я0-9-_]{${MIN_LOGIN_LENGTH},}$`);
+  const pattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
   return pattern.test(value);
 };
 
@@ -16,7 +16,7 @@ const isPasswordValid = (value: string) => {
 export function validateEmail(email: ElementCreator, errorDiv: ElementCreator): boolean {
   const emailValue = (email.getNode() as HTMLInputElement).value;
   const isValid = isEmailValid(emailValue);
-  errorDiv.setTextContent(isValid ? '' : 'Username must start with a capital letter and be at least 3 characters long');
+  errorDiv.setTextContent(isValid ? '' : 'Correct format: name@example.com');
   return isValid;
 }
 
@@ -26,7 +26,7 @@ export function validatePassword(password: ElementCreator, errorDiv: ElementCrea
   errorDiv.setTextContent(
     isValid
       ? ''
-      : 'Password must be at least 6 characters, include a small letter, a capital letter, a number and a special symbol'
+      : 'Password must be at least 8 characters, include a small letter, a capital letter, a number and a special symbol'
   );
   return isValid;
 }
