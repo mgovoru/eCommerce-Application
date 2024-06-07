700abf102f418d72abc55bcd59dc715098ddb9ee
Lex Mel <lex07672@gmail.com>
2024-06-07 15:46:25 +0300

    feat: test registration page pasword validation

--- src/__tests__/validation.page-registration.test.ts
+++ src/__tests__/validation.page-registration.test.ts
@@ -7,6 +7,7 @@ describe('RegistrationValidation', () => {
     validation = new RegistrationValidation();
   });
 
+  // тест ввода почты
   describe('validMail', () => {
     test('should return true for a valid email', () => {
       const response = validation.validMail('test@example.com');
@@ -36,6 +37,44 @@ describe('RegistrationValidation', () => {
       const response = validation.validMail('test@exa*mple.com');
       expect(response.result).toBe(false);
       expect(response.error).toBe('Correct format: name@example.com');
+    });
+  });
+
+  // тест ввода пароля
+  describe('validPassword', () => {
+    test('should return true for a valid password', () => {
+      const response = validation.validPassword('Valid1Password');
+      expect(response.result).toBe(true);
+    });
+
+    test('should return false for a password less than 8 characters', () => {
+      const response = validation.validPassword('Va1P');
+      expect(response.result).toBe(false);
+    });
+
+    test('should return false for a password without a lowercase letter', () => {
+      const response = validation.validPassword('VALID1PASSWORD');
+      expect(response.result).toBe(false);
+    });
+
+    test('should return false for a password without an uppercase letter', () => {
+      const response = validation.validPassword('valid1password');
+      expect(response.result).toBe(false);
+    });
+
+    test('should return false for a password without a number', () => {
+      const response = validation.validPassword('ValidPassword');
+      expect(response.result).toBe(false);
+    });
+
+    test('should return true for a password with special characters', () => {
+      const response = validation.validPassword('Valid1Password!');
+      expect(response.result).toBe(true);
+    });
+
+    test('should return false for a password with invalid characters', () => {
+      const response = validation.validPassword('Valid1Passwor👁️');
+      expect(response.result).toBe(false);
     });
   });
 });
