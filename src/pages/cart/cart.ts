b287171ad345d8482bffe83401ed779418606453
mariaklyass <klyassmaria@gmail.com>
2024-06-15 14:17:14 +0300

    feat: add styling and fix bugs

--- src/pages/cart/cart.ts
+++ src/pages/cart/cart.ts
@@ -7,6 +7,7 @@ import State from '../../state/state';
 import ErrorView from '../../server/error';
 import { ElementCreator } from '../../app/base';
 import './cart.scss';
+import { Pages } from '../../router/pages';
 
 function getLocalizedString(localizedString: LocalizedString, locale: string = 'en'): string {
   return localizedString[locale] || Object.values(localizedString)[0];
@@ -45,9 +46,19 @@ export default class CartView extends View {
 
     const cartItemsContainer = this.drawElement({ tag: 'div', classNames: ['page-cart__items'] }, cartContainer);
 
-    const clearCartButton = this.drawElement({ tag: 'button', classNames: ['page-cart__clear'] }, cartContainer);
+    const totalCostContainer = this.drawElement({ tag: 'div', classNames: ['page-cart__total'] }, cartContainer);
+    this.updateTotalCost(totalCostContainer);
+
+    const cartButtonsContainer = this.drawElement({ tag: 'div', classNames: ['page-cart__buttons'] }, cartContainer);
+    const clearCartButton = this.drawElement({ tag: 'button', classNames: ['page-cart__clear'] }, cartButtonsContainer);
     clearCartButton.textContent = 'Clear Shopping Cart';
+    const proceedToOrderButton = this.drawElement(
+      { tag: 'button', classNames: ['page-cart__order'] },
+      cartButtonsContainer
+    );
+    proceedToOrderButton.textContent = 'Place the order';
     // clearCartButton.addEventListener('click', () => this.clearCart());
+    // proceedToOrderButton.addEventListener('click', () => this.showSuccessModal());
 
     this.renderCartItems(cartItemsContainer);
 
@@ -78,6 +89,7 @@ export default class CartView extends View {
         emptyMessage.textContent = 'Your cart is empty.';
         const linkToCatalog = this.drawElement({ tag: 'button', classNames: ['page-cart__button'] }, container);
         linkToCatalog.textContent = 'Try to find something you like here ->';
+        linkToCatalog.addEventListener('click', () => this.router.navigate(Pages.SHOP));
       } else {
         cartItems?.forEach((item) => {
           const itemElement = this.createCartItemElement(item);
@@ -98,19 +110,29 @@ export default class CartView extends View {
     const itemName = this.drawElement({ tag: 'div', classNames: ['cart-item__name'] }, itemElement);
     itemName.textContent = getLocalizedString(item.name);
 
+    const itemImage = this.drawElement(
+      { tag: 'img', classNames: ['cart-item__image'] },
+      itemElement
+    ) as HTMLImageElement;
+    itemImage.src =
+      item.variant.images && item.variant.images.length > 0 ? item.variant.images[0]?.url : 'path/to/default/image.jpg';
+    itemImage.alt = getLocalizedString(item.name);
+
     const itemQuantity = this.drawElement({ tag: 'div', classNames: ['cart-item__quantity'] }, itemElement);
     itemQuantity.textContent = `Quantity: ${item.quantity}`;
 
+    const quantityButtons = this.drawElement({ tag: 'div', classNames: ['cart-item__quantity-buttons'] }, itemElement);
+
     const increaseButton = this.drawElement(
       { tag: 'button', classNames: ['cart-item__button', 'cart-item__increase'] },
-      itemElement
+      quantityButtons
     );
     increaseButton.textContent = '+';
     // increaseButton.addEventListener('click', () => this.handleQuantityChange(item.id, 'increase'));
 
     const decreaseButton = this.drawElement(
       { tag: 'button', classNames: ['cart-item__button', 'cart-item__decrease'] },
-      itemElement
+      quantityButtons
     );
     decreaseButton.textContent = '-';
     // decreaseButton.addEventListener('click', () => this.handleQuantityChange(item.id, 'decrease'));
@@ -121,7 +143,7 @@ export default class CartView extends View {
 
     const removeButton = this.drawElement({ tag: 'button', classNames: ['cart-item__remove'] }, itemElement);
     removeButton.textContent = 'Remove';
-    // removeButton.addEventListener('click', () => this.handleRemoveItem(item.id));
+    removeButton.addEventListener('click', () => this.handleRemoveItem(item.id));
 
     return itemElement;
   }
@@ -136,15 +158,89 @@ export default class CartView extends View {
   //   }
   // }
 
-  // async handleRemoveItem(itemId: number) {
-  //   try {
-  //     await this.requestCart.removeItemFromCart(itemId);
-  //     this.updateView();
-  //   } catch (err) {
-  //     const errorElement = new ErrorView();
-  //     errorElement.show(err.message);
-  //   }
-  // }
+  async handleRemoveItem(itemId: string) {
+    const cartID = localStorage.getItem('idCart');
+    const cartVersionString = localStorage.getItem('idCartVersionAnonimus');
+    const cartVersion = cartVersionString ? Number(cartVersionString) : null;
+
+    if (!cartID || cartVersion === null || isNaN(cartVersion)) {
+      const errorElement = new ErrorView();
+      errorElement.show('Invalid cart ID or version');
+      return;
+    }
+    if (!this.server.workApi?.userApi) {
+      try {
+        const response = await this.server
+          .apiRoot()
+          .carts()
+          .withId({ ID: cartID })
+          .post({
+            body: {
+              version: this.server.versionCartAnonimus,
+              actions: [{ action: 'removeLineItem', lineItemId: itemId }],
+            },
+          })
+          .execute();
+        this.server.versionCartAnonimus = response?.body.version as number;
+        localStorage.setItem('idCartVersionAnonimus', JSON.stringify(response.body.version));
+      } catch (err) {
+        const errorElement = new ErrorView();
+        errorElement.show(err as string);
+      }
+    } else if (this.server.workApi?.userApi) {
+      try {
+        const response = await this.server.workApi.userApi
+          .apiRoot()
+          ?.me()
+          .carts()
+          .withId({ ID: cartID })
+          .post({
+            body: {
+              version: cartVersion,
+              actions: [
+                {
+                  action: 'removeLineItem',
+                  lineItemId: itemId,
+                },
+              ],
+            },
+          })
+          .execute();
+        this.server.versionCartLogin = response?.body.version as number;
+      } catch (err) {
+        const errorElement = new ErrorView();
+        errorElement.show(err as string);
+      }
+    }
+  }
+
+  async updateTotalCost(container: HTMLElement) {
+    const cartID = localStorage.getItem('idCart');
+    if (!cartID) {
+      container.textContent = 'Total Cost: $0.00';
+      return;
+    }
+
+    this.server
+      .apiRoot()
+      .carts()
+      .withId({ ID: cartID })
+      .get()
+      .execute()
+      .then((fetchCartResult) => {
+        const cartItems = fetchCartResult?.body.lineItems;
+        if (cartItems) {
+          const totalCost = cartItems.reduce((acc, item) => acc + item.price.value.centAmount * item.quantity, 0) / 100;
+          container.textContent = `Total Cost: $${totalCost.toFixed(2)}`;
+        } else {
+          container.textContent = 'Total Cost: $0.00';
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+        container.textContent = 'Total Cost: $0.00';
+      });
+  }
 
   updateView() {
     const container = this.viewElementCreator.getNode().querySelector('.page-cart__items') as HTMLElement;
